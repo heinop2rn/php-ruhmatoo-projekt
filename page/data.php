a2baa427a159baf800d0305b62171ccc6af4a89d
Heino Pärn <heinop2rn@users.noreply.github.com>
2016-12-07 10:44:18 +0200

    data lehele uploadimise form

--- page/data.php
+++ page/data.php
@@ -117,6 +117,12 @@
 			
 </form>
 
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    Select image to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload Image" name="submit">
+</form>
+
 <h2>Ordered products:</h2>
 <?php
 
