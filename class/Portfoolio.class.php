4108242ae9dde4fdd99bb0581534235a470b6ba2
Sirje Maria Lehestik <sirjemaria@users.noreply.github.com>
2017-01-08 22:11:46 +0200

    Portfoolio leht

--- class/Portfoolio.class.php
+++ class/Portfoolio.class.php
@@ -8,9 +8,29 @@ class Portfoolio {
 	function __construct ($mysqli) {
 		
 		$this->connection = $mysqli;
-	
+		
+		function savePortfolio ($facebooki_leht, $blogi, $portfoolio, $kirjeldus){
+		
+			$database = "if16_sirjemaria";
+			$this->connection = new $this->connection($GLOBALS["serverHost"],$GLOBALS["serverUsername"],$GLOBALS["serverPassword"],$GLOBALS["database"]);
+		
+			$stmt = $this->connection->prepare("INSERT INTO users (facebooki_leht, blogi, portfoolio, kirjeldus) VALUES (?, ?, ?, ?)");
+			echo $this->connection->error;
+		
+			$stmt->bind_param("ssss", $facebooki_leht, $blogi, $portfoolio, $kirjeldus);
+		
+			if ($stmt->execute()) {
+				echo "Saved!";
+			} else {
+				echo "ERROR".$stmt->error;
+	   }
+	   
+		$stmt->close();
+		$this->connection->close();
+		}
 	}
 }
+
 	
 	
 	
--- page/portfoolio.php
+++ page/portfoolio.php
@@ -2,17 +2,12 @@
 
 	require("../functions.php");
 	
-	require("../class/Car.Class.php");
-	$Car = new Car($mysqli);
 	
 	require("../class/Portfoolio.class.php");
 	$Portfoolio = new Portfoolio($mysqli);
 	
 	require("../class/Helper.Class.php");
 	$Helper = new Helper($mysqli);
-	
-	require("../class/Order.Class.php");
-	$Order = new Order($mysqli);
 	
 	$PictureError = "";
 	
@@ -40,37 +35,69 @@
 		unset($_SESSION["message"]);
 	}
 	
-	
-	if(isset($_POST["insert"]))  {
-		if ( empty ( $_POST["insert"] ) ) {
+	if(isset($_POST["facebooki_leht"]) &&
+ 			isset($_POST["blogi"]) &&
+			isset($_POST["portfoolio"]) &&
+			isset($_POST["kirjeldus"]) &&
+ 			!empty($_POST["facebooki_leht"]) &&
+			!empty($_POST["blogi"]) &&
+			!empty($_POST["portfoolio"]) &&
+ 			!empty($_POST["kirjeldus"])
+  		) {
 			
-
-			$PictureError = "<i>Pilti ei sisestatud!</i>";
-			
-		} else {
-		
-			$connect = $_POST["insert"];
-			
-		}
+			$Portfoolio->savePortfolio($Helper->cleanInput($_POST["facebooki_leht"]), $Helper->cleanInput($_POST["blogi"]), $Helper->cleanInput($_POST["portfoolio"]), $Helper->cleanInput($_POST["kirjeldus"]));
 	}
 	
 	
-		
- 
-    $query = "SELECT * FROM images ORDER BY id DESC";  
-    $result = mysqli_query($query, $connect);  
-    while($row = mysqli_fetch_array($result))  
-    {  
-		echo '  
-			<tr>  
-				<td>  
-					<img src="data:image/jpeg;base64,'.base64_encode($row['name'] ).'" height="200" width="200" class="img-thumnail" />  
-				</td>  
-			</tr>  
-		';  
-	}    
-	
-	
+if(isset($_FILES["fileToUpload"]) && !empty($_FILES["fileToUpload"]["name"])){
+    $target_dir = "uploads/";
+    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+    // Check if image file is a actual image or fake image
+    if(isset($_POST["submit"])) {
+        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+        if($check !== false) {
+            echo "File is an image - " . $check["mime"] . ".";
+            $uploadOk = 1;
+        } else {
+            echo "File is not an image.";
+            $uploadOk = 0;
+        }
+    }
+    // Check if file already exists
+    if (file_exists($target_file)) {
+        echo "Sorry, file already exists.";
+        $uploadOk = 0;
+    }
+    // Check file size
+    if ($_FILES["fileToUpload"]["size"] > 500000) {
+        echo "Sorry, your file is too large.";
+        $uploadOk = 0;
+    }
+    // Allow certain file formats
+    if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+    && $imageFileType != "gif" ) {
+        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+        $uploadOk = 0;
+    }
+    // Check if $uploadOk is set to 0 by an error
+    if ($uploadOk == 0) {
+        echo "Sorry, your file was not uploaded.";
+    // if everything is ok, try to upload file
+    } else {
+        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+            echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+            
+            // save file name to DB here
+            
+        } else {
+            echo "Sorry, there was an error uploading your file.";
+        }
+    }
+}else{
+    echo "Please select the file that you want to upload!";
+}
 ?>
 <?php require("../partials/header.php");?>
 
@@ -94,12 +121,11 @@
 			</div>
 			<div class="form-group">
 				<input class="form-control" type="text" name="Portfoolioaddress" placeholder="Portfoolio link">
-			
 			<br>
 			<form action="demo_form.asp" id="usrform">
 			<textarea name="comment" form="usrform" rows="5" cols="35">Kirjelda ennast...</textarea>
+			<input type="submit" value="Lisa info">
 			</div>
-			
 		</div>
 	</div>
 	</div>
@@ -107,48 +133,22 @@
 </html>
 </form>
 </form>
- <!DOCTYPE html>  
- <html>  
-      <body>  
-           <br /><br />  
-           <div class="container" style="width:500px;">   
-                <form method="post" enctype="multipart/form-data">  
-                     <input type="file" name="image" id="image" />  
-                     <br />  
-                     <input type="submit" name="insert" id="insert" value="Insert" class="btn btn-info" />  
-                </form>  
-                <br />  
-                <br />  
-                <table class="table table-bordered">  
-                     <tr>  
-                          <th>Image</th>  
-                     </tr>  
-                </table>  
-           </div>  
-      </body>  
- </html>  
- <script>  
- $(document).ready(function(){  
-      $('#insert').click(function(){  
-           var image_name = $('#image').val();  
-           if(image_name == '')  
-           {  
-                alert("Please Select Image");  
-                return false;  
-           }  
-           else  
-           {  
-                var extension = $('#image').val().split('.').pop().toLowerCase();  
-                if(jQuery.inArray(extension, ['gif','png','jpg','jpeg']) == -1)  
-                {  
-                     alert('Invalid Image File');  
-                     $('#image').val('');  
-                     return false;  
-                }  
-           }  
-      });  
- });  
- </script> 
+<!DOCTYPE html>
+<html>
+<body>
+<div class="container">
+		<div class="row">
+			<div class="col-sm-4 col-md-3 col-sm-offset-4 col-md-offset-3">
+			<form action="upload.php" method="post" enctype="multipart/form-data">
+			Sisesta portfoolio pildid:
+			<input type="file" name="fileToUpload" id="fileToUpload">
+			<input type="submit" value="Upload Image" name="submit">
+		</div>
+	</div>
+</form>
+
+</body>
+</html>
 
 <h2>Profiil</h2>
 
